e3d0f40f24fd74029679fc25f12fca0616b5e71b
zhuchunyi <zhuchunyi@huawei.com>
2020-07-31 20:43:33 +0800

    submit the source code of oemaker

---
+++ make_debug.sh
@@ -0,0 +1,61 @@
+:<<!
+ * Copyright (c) Huawei Technologies Co., Ltd. 2018-2019. All rights reserved.
+ * iSulad licensed under the Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *     http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+ * PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ * Author: zhuchunyi
+ * Create: 2020-08-05
+ * Description: provide container buffer functions
+!
+
+#!/bin/bash
+
+CONFIG_DBG_LIST="config/standard/debug_rpmlist"
+
+set -e
+function down_ava_debug_rpm()
+{
+    [ -d "$DBG_DIR" ] && rm -rf "$DBG_DIR"
+    yum list --installroot="${BUILD}"/tmp available | awk '{print $1}' | grep -E 'devel|debuginfo' | grep -v "i686" > ava_deb_lst
+    local yumdownloader_log_startline=$(($(awk 'END{print NR}' /var/log/dnf.log)+1))
+    yumdownloader --installroot="${BUILD}"/tmp --destdir="$DBG_DIR" $(cat ava_deb_lst | tr '\n' ' ') > /dev/null
+    if [ $? -ne 0 ] || sed -n ''${yumdownloader_log_startline}',$p' /var/log/dnf.log | grep -n 'conflicting requests'; then
+        return 1
+    fi
+    return 0
+}
+
+function get_debug_rpm()
+{
+    rm -rf debug_rpm_lst
+    set +e
+    rpm -qpi "$BUILD"/iso/Packages/*.rpm | grep "Source RPM" | awk '{print $4}' | sort | uniq > iso_src_lst
+    for debug_rpm in $(ls "$DBG_DIR" | grep rpm$)
+    do
+        src_name=$(rpm -qpi "$DBG_DIR"/"$debug_rpm" | grep "Source RPM" | awk '{print $4}')
+        grep "^$src_name$" iso_src_lst > /dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            [ -n "$debug_rpm" ] && rm -rf "$DBG_DIR"/"$debug_rpm"
+        else
+            rpm -qp --qf "%{NAME}.%{ARCH}\n" "$DBG_DIR"/"$debug_rpm" >> debug_rpm_lst
+        fi
+    done
+    set -e
+    local yumdownloader_log_startline=$(($(awk 'END{print NR}' /var/log/dnf.log)+1))
+    yumdownloader --resolve --installroot="${BUILD}"/tmp --destdir="$DBG_DIR" $(cat debug_rpm_lst | tr '\n' ' ')
+    if [ $? -ne 0 ] || sed -n ''${yumdownloader_log_startline}',$p' /var/log/dnf.log | grep -n 'conflicting requests'; then
+        return 1
+    fi
+    ls "${BUILD}"/iso/Packages/ | sort > iso_lst
+    ls "$DBG_DIR"/ |sort > deb_lst
+    for del_rpm in $(cat iso_lst deb_lst | sort -n | uniq -d)
+    do
+        [ -n "$del_rpm" ] && rm -rf "$DBG_DIR"/"$del_rpm"
+    done
+    return 0
+}
---
+++ oemaker.sh
@@ -0,0 +1,118 @@
+:<<!
+ * Copyright (c) Huawei Technologies Co., Ltd. 2018-2019. All rights reserved.
+ * iSulad licensed under the Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *     http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+ * PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ * Author: zhuchunyi
+ * Create: 2020-08-05
+ * Description: provide container buffer functions
+!
+
+#!/bin/bash
+
+set -e
+cd /opt/oemaker
+REPOS=""
+export OUTPUT_DIR="/result"
+export CPATH=$(pwd)
+
+umask 0022
+
+source "${CPATH}"/init.sh
+source "${CPATH}"/iso.sh
+source "${CPATH}"/rpm.sh
+source "${CPATH}"/img_repo.sh
+
+function mkclean()
+{
+    rm -rf /etc/yum/repos.d/*
+    [ -n "${BUILD}" ] && rm -rf "${BUILD}"
+    rm -rf /etc/yum.repos.d && mv repos.old /etc/yum.repos.d
+}
+
+function mk_euleros_main()
+{
+    echo "Parsing cmd line..."
+    parse_cmd_line "$@"
+    if [ $? -ne 0 ]; then
+        echo "parse params error"
+        return 1
+    fi
+
+    echo "Initializing variables..."
+    global_var_init
+    if [ $? -ne 0 ]; then
+        echo "init env failed"
+        return 1
+    fi
+
+    create_install_img &
+
+    echo "Creating repos..."
+    create_repos
+    if [ $? -ne 0 ]; then
+        echo "create repo failed"
+        return 1
+    fi
+
+    echo "Downloading rpms..."
+    download_rpms
+    if [ $? -ne 0 ]; then
+        echo "down rpms failed"
+        return 1
+    fi
+
+    echo "Initializing config..."
+    wait && cat lorax.logfile
+    init_config
+    if [ $? -ne 0 ]; then
+        echo "init config failed"
+        return 1
+    fi
+
+    echo "Getting rpm public key..."
+    get_rpm_pub_key
+    if [ $? -ne 0 ]; then
+        echo "get rpm pub key failed"
+        return 1
+    fi
+
+    echo "Waiting for lorax to finish..."
+    if [ "${ISOTYPE}" == "debug" ]; then
+        gen_debug_iso
+        if [ $? -ne 0 ]; then
+            echo "create debug iso failed"
+            return 1
+        fi
+        mkclean
+        echo "${OUTPUT_DIR}/${DBG_ISO_NAME}"
+    elif [ "${ISOTYPE}" == "standard" ]; then
+        gen_iso
+        if [ $? -ne 0 ]; then
+            echo "create install iso failed"
+            return 1
+        fi
+        mkclean
+        echo "${OUTPUT_DIR}/${ISO_NAME}"
+   elif [ "${ISOTYPE}" == "source" ]; then
+        gen_src_iso
+        if [ $? -ne 0 ]; then
+            echo "create source iso failed"
+            return 1
+        fi
+        mkclean
+        echo "${OUTPUT_DIR}/${SRC_ISO_NAME}"
+    fi
+    return 0
+}
+
+mk_euleros_main "$@"
+if [ $? -ne 0 ]; then
+    echo "make iso failed"
+    exit 1
+fi
