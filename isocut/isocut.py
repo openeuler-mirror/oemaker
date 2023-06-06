7a6d88daf32202b0bb0b96d670db4ca943c58af1
瑞哥 <lqopr@hotmail.com>
2023-06-06 10:26:56 +0000

    inst.ks使用hd:LABEL=格式，不再使用兼容性不佳cdrom关键字
    Signed-off-by: 瑞哥 <lqopr@hotmail.com>

--- isocut/isocut.py
+++ isocut/isocut.py
@@ -626,14 +626,14 @@ def replace_kickstart_file():
         return 13
 
     if os.uname()[-1].strip() == 'x86_64':
-        sed_cmd = r"sed -i '/append/ s/$/ inst.ks=cdrom:\/dev\/cdrom:\/" + KS_NAME + \
+        sed_cmd = r"sed -i '/append/ s/$/ inst.ks=hd:LABEL=" + ICONFIG.new_iso_name + ":\/" + KS_NAME + \
             " inst.multilib/g' " + ICONFIG.temp_path_new_image + "/" + ISOLINUX_CFG
         ret = ICONFIG.run_cmd(sed_cmd)
         if ret[0] != 0:
             print("Set kickstart file failed!!")
             return 13
 
-    sed_cmd = r"sed -i '/inst.stage2/ s/$/ inst.ks=cdrom:\/dev\/cdrom:\/" + KS_NAME + \
+    sed_cmd = r"sed -i '/inst.stage2/ s/$/ inst.ks=hd:LABEL=" + ICONFIG.new_iso_name + ":\/" + KS_NAME + \
         " inst.multilib/g' " + ICONFIG.temp_path_new_image + "/" + EFILINUX_CFG
     ret = ICONFIG.run_cmd(sed_cmd)
     if ret[0] != 0:
