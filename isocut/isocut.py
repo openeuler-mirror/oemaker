c5d8d69f33de9b9d874449397145b508b1d980de
瑞哥 <lqopr@hotmail.com>
2023-12-15 17:45:53 +0800

    调整书写格式,符合CI规范 1.G.FMT.02 行宽不超过120个字符 2.G.FMT.04 用空格突出关键字和重要信息--分隔符

--- isocut/isocut.py
+++ isocut/isocut.py
@@ -626,8 +626,9 @@ def replace_kickstart_file():
         return 13
 
     if os.uname()[-1].strip() == 'x86_64':
-        get_stage2_cmd = r"sed -n '0,/^.*inst.stage2=\([^ ]*\) .*$/ s/^.*inst.stage2=\([^ ]*\) .*$/\1/p' " + ICONFIG.temp_path_new_image + "/" + ISOLINUX_CFG
-        ret,sout = ICONFIG.run_cmd(get_stage2_cmd)
+        get_stage2_cmd = r"sed -n '0,/^.*inst.stage2=\([^ ]*\) .*$/ s/^.*inst.stage2=\([^ ]*\) .*$/\1/p' " + \
+            ICONFIG.temp_path_new_image + "/" + ISOLINUX_CFG
+        ret, sout = ICONFIG.run_cmd(get_stage2_cmd)
         if ret != 0:
             print("Set kickstart file failed!!")
             return 13
@@ -638,8 +639,9 @@ def replace_kickstart_file():
             print("Set kickstart file failed!!")
             return 13
 
-    get_stage2_cmd = r"sed -n '0,/^.*inst.stage2=\([^ ]*\) .*$/ s/^.*inst.stage2=\([^ ]*\) .*$/\1/p' " + ICONFIG.temp_path_new_image + "/" + ISOLINUX_CFG
-    ret,sout = ICONFIG.run_cmd(get_stage2_cmd)
+    get_stage2_cmd = r"sed -n '0,/^.*inst.stage2=\([^ ]*\) .*$/ s/^.*inst.stage2=\([^ ]*\) .*$/\1/p' " + \
+        ICONFIG.temp_path_new_image + "/" + ISOLINUX_CFG
+    ret, sout = ICONFIG.run_cmd(get_stage2_cmd)
     if ret != 0:
         print("Set efi kickstart file failed!!")
         return 13
